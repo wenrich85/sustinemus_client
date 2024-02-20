56d9264713b1e9fa8167bd63b9debb4f02fbda14
wenrich85 <wenrich85@gmail.com>
2024-02-20 01:19:18 -0500

    config4

--- config/runtime.exs
+++ config/runtime.exs
@@ -40,13 +40,14 @@ if config_env() == :prod do
     server: true,
     url: [host: host, port: 443, scheme: "https"],
     http: [
+      transport_options: [:socket_opts [:inet6]],
       port: port,
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
+    ],
+    check_origin: [
+      "https://sustinemus.com",
+      "https://www.sustinemus.com",
+      "http://sustinemus.com#{port}",
+      "http://www.sustinemus.com#{port}"
     ],
     secret_key_base: secret_key_base
 
